3b1ff3c4cd042b2c8fc1cf16977d90e23f439590
javitocor <56809963+javitocor@users.noreply.github.com>
2020-04-06 10:46:43 +0200

    Added fibonacci even solution

--- JS/fibonacci-even.js
+++ JS/fibonacci-even.js
@@ -2,7 +2,7 @@ function fibonacci_even(number){
     var arr = [1,2];
     var count = 0;
     var count2 = 2;
-    for (var i = 1; i < number; i++){
+    for (var i = 1; count <= number; i++){
         count = arr[i] + arr[i-1];
         arr.push(count);
         if (count % 2 == 0){
@@ -12,3 +12,19 @@ function fibonacci_even(number){
     return count2;
 }
 
+
+function fibonacci_even(number){
+    var first = 0;
+    var second = 1;
+    var sum = 0;
+    var count = 0;
+    while (count <= number){
+        count = first + second;
+        if (count%2 == 0){
+            sum += count;
+        } 
+        first = second;
+        second = count;
+    }
+    return sum;
+}
